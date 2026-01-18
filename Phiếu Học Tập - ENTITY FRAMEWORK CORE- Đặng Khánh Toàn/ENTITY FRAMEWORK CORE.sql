77fd742def3a6b1bd783bf0e6ac9c1f0af19ad8f
Đặng Khánh Toàn <dangkhanhtoan206@gmail.com>
2026-01-18 23:07:55 +0700

    Add files via upload

---
+++ Phiếu Học Tập - ENTITY FRAMEWORK CORE- Đặng Khánh Toàn/ENTITY FRAMEWORK CORE.sql
@@ -0,0 +1,42 @@
+﻿
+IF NOT EXISTS (SELECT name FROM sys.databases WHERE name = 'EShopDb')
+BEGIN
+    CREATE DATABASE EShopDb;
+END
+GO
+
+USE EShopDb;
+GO
+
+IF OBJECT_ID('Categories', 'U') IS NOT NULL
+    DROP TABLE Categories;
+GO
+
+CREATE TABLE Categories (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    Description NVARCHAR(255)
+);
+GO
+IF OBJECT_ID('Products', 'U') IS NOT NULL
+    DROP TABLE Products;
+GO
+
+CREATE TABLE Products (
+    Id INT IDENTITY(1,1) PRIMARY KEY,
+    Name NVARCHAR(100) NOT NULL,
+    Price DECIMAL(18,2) NOT NULL,
+    Stock INT NOT NULL,
+    CategoryId INT NOT NULL,
+    CONSTRAINT FK_Products_Categories
+        FOREIGN KEY (CategoryId)
+        REFERENCES Categories(Id)
+);
+GO
+SELECT 
+    COLUMN_NAME AS [Tên cột],
+    DATA_TYPE AS [Kiểu dữ liệu],
+    IS_NULLABLE AS [Cho phép NULL]
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'Products';
+GO
